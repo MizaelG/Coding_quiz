8675224df141ba1ef1c44548c453a521e5b78434
Mizael <mizaelgonzalez97@gmail.com>
2023-03-16 23:54:23 -0400

    initial

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Coding_quiz
+
+## Description
+
+This Assigment was to make a coding quiz. In this challenge i had to make a quiz with questions and multiple choice answers. As well as a timer. Everytime the correct answer was chose you will recieve a green checkmark stating that you got it correct. However if you were to choose the wrong answer you would get a red X. Their is up to 5 questions being asked.
+
+## Installation
+
+N/A
+
+## Usage
+
+Challenging the mind for anyone who is getting into JavaScript.
+
+## URL
+
+Background img used from google.  url(https://images.hdqwalls.com/download/simple-background-2560x1440.jpg)
+## Credits
+
+N/A
+
+## License
+
+MIT License
---
+++ script.js
@@ -0,0 +1,62 @@
+var start_btn = document.querySelector(".start_btn button");
+var quiz_box = document.querySelector
+
+
+const quiz_box[
+
+    question: "Javascript is an _______ language?",
+    {
+        
+        answers:
+        a: "Object-Orienteded",
+        b: "Object-Base",
+        c: "Procedural",
+        d: "None of the above",
+        correctAnswer: 'a'
+    },
+    
+        question: "How can a datatype be declared to be a constant type?",
+    {
+        answers:{ 
+        a: "const",
+        b: "var",
+        c: "let",
+        d: "constant",
+        correctAnswer: "a"
+        }
+    
+    },
+    question: "What does the JavaScript 'debugger' statement do?",
+    {
+        answers: 
+        a: "It will debug all the errors in the program at runtime",
+        b: "It acts as a breakpoint in a program.",
+        c: "It will debug error in the current statement if any.",
+        d: "All the above",
+        correctAnswer: "b"
+        
+    },
+    {
+        question:"Upon encountering empty statements, what does the JavaScript interpreter d?",
+        
+        answers: 
+        a: "Throws an error",
+        b: "Ignores the statements",
+        c: "Gives a warning",
+        d: "None of the above",
+        correctAnswer: "b"
+    },
+    {
+        question: "Which of the following are closures in JavaScript?",
+        answers: 
+            a: "Variables",
+            b: "Functions",
+            c: "Objects",
+            d: "All of the above",
+            correctAnswer: "d"
+        
+    
+    },
+]
+    
+        
